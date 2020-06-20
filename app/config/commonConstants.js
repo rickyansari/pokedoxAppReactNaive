5c8da9518ac5f2b28ea3d8eeb25dd500cfba782b
Techsophy <techsophy@mayur.d>
2020-06-20 14:22:43 +0530

    more code removed

--- app/config/commonConstants.js
+++ app/config/commonConstants.js
@@ -1,22 +1,3 @@
-const UIConstants = {
-  APP_HEADER: 'appHeader',
-  BUTTON_TEXT: 'buttonText',
-  DEFAULT_INPUT_TEXT: 'inputText',
-  DEFAULT_TEXT: 'defaultText',
-  INPUT_HEADER: 'inputHeader',
-  LIGHT_GRAY_TEXT: 'lightGrayText',
-  LOGIN_HEADER: 'loginHeader',
-  LOGOUT_TEXT: 'logoutText',
-  MATERIAL_TAB_TEXT: 'materialTabText',
-  MATERIAL_TITLE: 'materialTitle',
-  PROFILE_MOBILE: 'profileMobile',
-  PROFILE_NAME: 'profileName',
-  PROFILE_TITLE: 'profileTitleText',
-  BRAND_CATEGORY: 'brandCategory',
-  BRAND_NAME: 'brandName',
-  CARD_HEADING: 'cardHeading',
-};
-
 const Messages = {
   commonExceptionMessage: 'unable to process request please try again letter.',
 };
@@ -28,6 +9,5 @@ const SCREEN_NAME = {
 
 module.exports = {
   ...Messages,
-  ...UIConstants,
   SCREEN_NAME,
 };
