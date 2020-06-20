4f495c43b14d1e10eaa8494dce3fd29ee188d68a
Techsophy <techsophy@mayur.d>
2020-06-20 14:27:05 +0530

    removed i18n

--- app/components/IBText.js
+++ app/components/IBText.js
@@ -2,29 +2,11 @@
 import React from 'react';
 import styled from 'styled-components';
 import {rs} from 'app/theme/responsiveSize';
-import {
-  APP_HEADER,
-  BUTTON_TEXT,
-  DEFAULT_INPUT_TEXT,
-  DEFAULT_TEXT,
-  INPUT_HEADER,
-  LIGHT_GRAY_TEXT,
-  LOGIN_HEADER,
-  LOGOUT_TEXT,
-  MATERIAL_TAB_TEXT,
-  MATERIAL_TITLE,
-  PROFILE_MOBILE,
-  PROFILE_NAME,
-  PROFILE_TITLE,
-  BRAND_CATEGORY,
-  BRAND_NAME,
-  CARD_HEADING,
-} from 'app/config/commonConstants';
-import i18n from 'app/config/i18n';
+import {DEFAULT_TEXT} from 'app/config/commonConstants';
+
 export default function IBText({
   textStyle = {},
   children,
-  shouldTranslate = true,
   type = DEFAULT_TEXT,
   ...restProps
 }) {
@@ -33,10 +15,10 @@ export default function IBText({
     default:
       TextComponent = defaultText;
   }
-  const textValue = shouldTranslate ? i18n.t(children) : children;
+
   return (
     <TextComponent style={{...textStyle}} {...restProps}>
-      {textValue}
+      {children}
     </TextComponent>
   );
 }
